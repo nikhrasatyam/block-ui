17f41eb5241757344c18919c26628d383f584bfa
Satyam Nikhra <neekhrasatyam1993@gmail.com>
2019-12-01 20:21:36 -0800

    blockList test case add

--- src/components/block/block.list.test.jsx
+++ src/components/block/block.list.test.jsx
@@ -11,7 +11,7 @@ import BlockList from './block.list';
 describe('BlockList Test Cases', () => {
   let uiStore;
   let blockStore;
-  beforeEach(async () => {
+  beforeEach(() => {
     uiStore = new UiStore();
     blockStore = new BlockStore();
   });
@@ -28,26 +28,26 @@ describe('BlockList Test Cases', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test('should display table and button when list is not empty', async () => {
+  test('should display table and button when list is not empty', () => {
     blockStore.setBlockList(blockListMock);
     const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
     expect(wrapper.find('Button')).toHaveLength(1);
     expect(wrapper.find('TableExpandable')).toHaveLength(1);
   });
 
-  test('should not display table and button when list is empty', async () => {
+  test('should not display table and button when list is empty', () => {
     const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
     expect(wrapper.find('Button')).toHaveLength(0);
     expect(wrapper.find('TableExpandable')).toHaveLength(0);
   });
 
-  test('should  display table and button when list is not empty', async () => {
+  test('should  display table and button when list is not empty', () => {
     const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
     expect(wrapper.find('Button')).toHaveLength(0);
     expect(wrapper.find('TableExpandable')).toHaveLength(0);
   });
 
-  test('should set the loading button disabled when showLoader is truthy', async () => {
+  test('should set the loading button disabled when showLoader is truthy', () => {
     blockStore.setBlockList(blockListMock);
     const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
     let button = wrapper.find('Button');
@@ -59,7 +59,7 @@ describe('BlockList Test Cases', () => {
     expect(button.props().disabled).toBeTruthy();
   });
 
-  test('should update the blockStore when loading button is clicked', async () => {
+  test('should update the blockStore when loading button is clicked', () => {
     blockStore.setBlockList(blockListMock);
     const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
     const button = wrapper.find('Button');
@@ -68,5 +68,25 @@ describe('BlockList Test Cases', () => {
     button.simulate('click');
     expect(blockStore.apiType.getBlockList).toBeTruthy();
     expect(blockStore.apiType.getAction).toBeTruthy();
+  });
+
+  test('should display table row', () => {
+    blockStore.setBlockList(blockListMock);
+    const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
+    expect(wrapper.find('TableExpandable')).toHaveLength(1);
+    expect(wrapper.find('RowAggregator')).toHaveLength(1);
+    expect(wrapper.find('td')).toHaveLength(3);
+  });
+
+  test('should display raw content when row is clicked', () => {
+    blockStore.setBlockList(blockListMock);
+    const wrapper = Enzyme.mount(<Provider uiStore={uiStore} blockStore={blockStore}><BlockList /></Provider>);
+    expect(wrapper.find('TableExpandable')).toHaveLength(1);
+    expect(wrapper.find('RowAggregator')).toHaveLength(1);
+    expect(wrapper.find('ExpandRow')).toHaveLength(0);
+    expect(wrapper.find('td')).toHaveLength(3);
+    wrapper.find('td').first().simulate('click');
+    const expandRow = wrapper.find('ExpandRow');
+    expect(expandRow).toHaveLength(1);
   });
 });
